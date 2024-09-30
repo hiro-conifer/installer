e6560296358bbd99977bd727f8e76204f568807d
Hirokey <132643478+hiro-conifer@users.noreply.github.com>
2024-09-30 13:40:10 +0900

    Add files via upload

---
+++ 5-otherpac.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# Value
+usernm=$1
+aur=yay
+sudoop="$usernm ALL=NOPASSWD: ALL"
+
+# arch-chroot
+arch-chroot /mnt << _EOF_
+
+# Enable nopassword
+echo ${sudoop} | sudo EDITOR='tee -a' visudo > /dev/null
+
+# Install AUR Helper
+su $usernm << __EOF__
+cd && git clone https://aur.archlinux.org/$aur.git
+cd $aur && makepkg -si --noconfirm
+__EOF__
+rm -rf /home/${usernm}/$aur
+
+# Install other packages
+pacman -S --noconfirm clamav ufw opendoas networkmanager pacman-contrib
+
+# Disable nopassword
+sudo sed -e "s/${sudoop}//g" /etc/sudoers | sudo EDITOR=tee visudo > /dev/null
+
+# Setting Packages
+# Clamav
+touch /var/log/clamav/freshclam.log && chown -R clamav:clamav /var/log/clamav
+freshclam
+
+# Ufw
+ufw default deny
+
+# Opendoas
+echo permit setenv {PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin} :wheel > /etc/doas.conf && chmod -c 0400 /etc/doas.conf
+
+# Enable Services
+systemctl enable {ufw,NetworkManager,systemd-{resolved,timesyncd},sshd}.service {paccache,fstrim}.timer
+
+_EOF_
