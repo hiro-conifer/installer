3e1939035b9f66791bb78ae29e754e1696168795
Hirokey <132643478+hiro-conifer@users.noreply.github.com>
2024-10-02 08:42:07 +0900

    Update 6-setupwm.sh

--- 6-setupwm.sh
+++ 6-setupwm.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
+# Value
+usernm=$1
+aur=yay
+sudoop="$usernm ALL=NOPASSWD: ALL"
 
-# Install Packages
-pacman -S --noconfirm vivaldi
+# arch-chroot
+arch-chroot /mnt << _EOF_
+
+# Enable nopassword
+echo ${sudoop} | EDITOR='tee -a' visudo > /dev/null
+
+# Install AUR Helper
+su $usernm << __EOF__
+$aur --noconfirm swayfx vivaldi
+__EOF__
+
+# Disable nopassword
+sudo sed -e "s/${sudoop}//g" /etc/sudoers | EDITOR=tee visudo > /dev/null
 
 # Vivaldi
 sed -i -e "s/\(Exec=\/usr\/bin\/vivaldi-stable\) \(%U\)/\1 --enable-features=UseOzonePlatform --ozone-platform=wayland --enable-wayland-ime \2/" /usr/share/applications/vivaldi-stable.desktop
