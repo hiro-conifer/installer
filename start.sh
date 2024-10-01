13357ffbe52f231e99c934108e14f8e990d5a240
Hirokey <132643478+hiro-conifer@users.noreply.github.com>
2024-10-02 08:45:04 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ source ./1-value.sh                                               # Type Value
 ./2-partition.sh $part $part_boot $part_root                      # Create Partitions
 ./3-pacstrap.sh $part_boot $part_root $ucode                      # Install Base Packages
 ./4-settings.sh $part_root $hostnm $usernm $userpw $rootpw $ucode # Other Setting
-./5-otherpac.sh $usernm                                           # Install Other Packages
+./5-otherpac.sh $usernm $aur                                      # Install Other Packages
