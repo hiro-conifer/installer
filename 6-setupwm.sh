d9b389313dac9c75d737ebd969f6e7a9965c0dba
Hirokey <132643478+hiro-conifer@users.noreply.github.com>
2024-10-02 14:24:38 +0900

    Update 6-setupwm.sh

--- 6-setupwm.sh
+++ 6-setupwm.sh
@@ -24,3 +24,5 @@ sudo sed -e "s/${sudoop}//g" /etc/sudoers | EDITOR=tee visudo > /dev/null
 
 # Vivaldi
 sed -i -e "s/\(Exec=\/usr\/bin\/vivaldi-stable\) \(%U\)/\1 --enable-features=UseOzonePlatform --ozone-platform=wayland --enable-wayland-ime \2/" /usr/share/applications/vivaldi-stable.desktop
+
+_EOF_
