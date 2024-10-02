278f8f7ddb249ddd1580998e1664ba95970ff1fc
Hirokey <132643478+hiro-conifer@users.noreply.github.com>
2024-10-02 11:50:46 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,3 +4,4 @@ source ./1-value.sh                                               # Type Value
 ./3-pacstrap.sh $part_boot $part_root $ucode                      # Install Base Packages
 ./4-settings.sh $part_root $hostnm $usernm $userpw $rootpw $ucode # Other Setting
 ./5-otherpac.sh $usernm $aur                                      # Install Other Packages
+./6-setupwm.sh $usernm $aur                                       # Setup SwayWM
